55b7935504a096a9a397a9bf1222a33472388b8c
cBasique <155490650+cBasique-v1@users.noreply.github.com>
2024-07-25 11:30:54 +0200

    ajout flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./scripts/**/*.js"],
+  content: ["./**/*.html", "./scripts/**/*.js, ./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
@@ -16,6 +16,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
   ],
 }
 
